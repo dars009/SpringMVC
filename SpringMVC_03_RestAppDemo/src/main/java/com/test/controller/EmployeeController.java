1592f8968d170648e4c769ff1e7cd11ea1b4d2ed
darshit <darshit.rabadiya@hotmail.com>
2023-04-28 09:43:42 +0530

    Path Param Removed !

--- SpringMVC_03_RestAppDemo/src/main/java/com/test/controller/EmployeeController.java
+++ SpringMVC_03_RestAppDemo/src/main/java/com/test/controller/EmployeeController.java
@@ -3,8 +3,6 @@ package com.test.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
